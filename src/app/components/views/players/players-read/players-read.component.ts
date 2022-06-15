5d02362f31fab5e20d2984f8bc5557ab7eb47336
Luis Eduardo <dupradosantini@opus-software.com.br>
2022-06-15 13:26:04 -0300

    Quick Display of Random Achievements works
    Player pages working aswell, small visual adjustments

--- src/app/components/views/players/players-read/players-read.component.ts
+++ src/app/components/views/players/players-read/players-read.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PlayerService } from '../player.service';
-import { Player } from '../players.model';
+import { Achievement, Player } from '../players.model';
 
 
 const DEFAULT_PAGE_NUMBER = 0;
@@ -34,8 +34,60 @@ export class PlayersReadComponent implements OnInit {
     .subscribe( response => {
       this.playerPage = response;
       this.playerArray = this.playerPage.content;
+      this.totalPages = response.totalPages;
       console.log(this.playerArray);
     })
   }
 
+  getRandomAchievements(achivements: Achievement[]): Achievement[]{
+    let newAchievArray:Achievement[] = [];
+    console.log("Achiev Length " + achivements.length);
+    if(achivements.length > 0){
+      //console.log(achivements.length);
+      const numbers = [];
+      const slice = 4;
+      let n,p;
+      for(let i = 0; i<slice; i++){
+        do{
+          n = Math.floor(Math.random() * achivements.length ) + 0;
+          p = numbers.includes(n);
+          if(!p){
+            numbers.push(n);
+            newAchievArray[i] = achivements[n];
+          }
+        }while(p);
+      }
+      return newAchievArray;
+    }else{
+      return newAchievArray;
+    }
+  }
+
+  /* PAGING METHODS */
+  counter(i: Number): Array<Number>{
+    return new Array(i);
+  }
+
+  goToPage(p: Number){
+    this.pageNumber = p;
+    this.getPlayerPage();
+  }
+
+  goToNext(){
+    if(this.pageNumber == (this.totalPages.valueOf() - 1)){
+      this.goToPage(this.pageNumber.valueOf());
+    }else{
+      this.pageNumber = this.pageNumber.valueOf() + 1;
+      this.goToPage(this.pageNumber.valueOf());
+    }
+  }
+
+  goToPrevious(){
+    if(this.pageNumber == 0){
+      this.goToPage(this.pageNumber.valueOf());
+    }else{
+      this.pageNumber = this.pageNumber.valueOf() - 1;
+      this.goToPage(this.pageNumber.valueOf());
+    }
+  }
 }
